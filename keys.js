f856b9cd59c0e7b3e6b8c84c826ef034ece8d914
Jonathan Dowd <jonathandowd@Jonathans-MacBook-Pro.local>
2018-04-21 18:53:07 -0400

    latest update

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.Bm3sxf7Yr9b2DM5vMxcJ1i6Gj,
-  consumer_secret: process.env.5o2gQcpAlvLZMZUtBMY9iMRbDJehaxMcu2v78izMtIdslOJaUv,
-  access_token_key: process.env.979075961911169030-zVw1TmjP7DqmrfJS833tVXSdKUxyoXZ,
-  access_token_secret: process.env.EcBmcYij8bYvBqzvFKGOwwAS1v5gdS0KSXhSlATdxuaQ5
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
